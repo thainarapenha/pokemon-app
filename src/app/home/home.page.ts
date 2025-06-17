9d1c854620b63183a06cadc4fc25290c61f7b074
thainarapenha <r15.thainarapenha@gmail.com>
2025-06-17 16:39:59 -0300

    feat: api pokemon list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { PokemonService } from '../services/http.service';
+import { PokemonCardComponent } from '../components/pokemon-card/pokemon-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, PokemonCardComponent],
 })
-export class HomePage {
-  constructor() {}
+
+
+export class HomePage implements OnInit {
+  public pokemons: any[] = [];
+  
+  constructor(private httpService: PokemonService) {}
+
+  ngOnInit() {
+    this.getAllPokemons();
+  }
+
+  getAllPokemons() {
+    this.httpService.getAllPokemon().subscribe(async (response: any) => {
+      const results = response.results;
+
+      const promises = results.map((p: any) =>
+        this.httpService.getPokemonName(p.name).toPromise()
+      );
+
+      const detailedPokemons = await Promise.all(promises);
+      this.pokemons = detailedPokemons.map((pokemon: any) => ({
+        id: pokemon.id,
+        name: pokemon.name,
+        types: pokemon.types.map((t: any) => t.type.name),
+        image: pokemon.sprites.other['official-artwork'].front_default
+      }));
+    });
+  }
 }
